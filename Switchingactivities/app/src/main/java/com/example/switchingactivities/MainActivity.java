d9ea75fa367bde8d1c95735b2694c14c815a8551
Sammonster495 <samarthshetty495@gmail.com>
2024-03-25 00:22:25 +0530

    feat: app to switch activities

---
+++ Switchingactivities/app/src/main/java/com/example/switchingactivities/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.switchingactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchActivity();
+            }
+        });
+    }
+
+    public void switchActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
