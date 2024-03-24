d8e3d0c2cff29e20b0a5aa6f7124e5ab82d5c568
Sammonster495 <samarthshetty495@gmail.com>
2024-03-25 00:26:59 +0530

    app to send SMS

---
+++ SendSMS/app/src/main/java/com/example/sendsms/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.sendsms;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    
+    private EditText text, phone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        
+        text = (EditText) findViewById(R.id.editTextText);
+        phone = (EditText) findViewById(R.id.editTextPhone);
+        Button button = findViewById(R.id.button);
+        
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSMS();
+            }
+        });
+    }
+
+    private void sendSMS() {
+        String phoneNumber = phone.getText().toString();
+        String message = text.getText().toString();
+
+        if (phoneNumber.isEmpty() || message.isEmpty()){
+            Toast.makeText(this, "Please enter the phone number and the message", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("sms_to:" + phoneNumber));
+        intent.putExtra("sms_body", message);
+
+        try {
+            startActivity(intent);
+        }catch (Exception e){
+            Toast.makeText(this, "Error sending SMS", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+}
